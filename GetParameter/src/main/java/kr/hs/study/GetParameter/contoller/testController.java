c6594804c03acd9f49723d2346fcadaf89cc07ef
Chaeyoung Lim <rnldyal1027@naver.com>
2023-05-24 12:13:42 +0900

    @PathVariable, RequestParam 연습!

--- GetParameter/src/main/java/kr/hs/study/GetParameter/contoller/testController.java
+++ GetParameter/src/main/java/kr/hs/study/GetParameter/contoller/testController.java
@@ -2,6 +2,8 @@ package kr.hs.study.GetParameter.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
@@ -46,8 +48,27 @@ public class testController {
         String b = wr.getParameter("data2");
         String[] c = wr.getParameterValues("data3");
         for(String str:c){
-            System.out.println("data3"+str);
+            System.out.println("data3 : "+str);
         }
+        return "result";
+    }
+
+    @GetMapping("/test4/{a}/{b}/{c}")
+    public String test4(@PathVariable String a, @PathVariable String b, @PathVariable String c){
+        System.out.println("data1 : "+a);
+        System.out.println("data2 : "+b);
+        System.out.println("data3 : "+c);
+        return "result";
+    }
+
+    @GetMapping("/test5")
+    public String test5(@RequestParam("data1") String data1, @RequestParam("data2") String data2, @RequestParam("data3") String [] data3){
+        System.out.println("data1 : "+data1);
+        System.out.println("data2 : "+data2);
+        for(String str:data3){
+            System.out.println("data3 : " + str);
+        }
+
         return "result";
     }
 }
